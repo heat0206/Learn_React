6de95cf0ad70fb589231b502176d000a2c167b9c
Heet <heetmankad06@gmail.com>
2026-06-03 14:47:43 +0530

    Formatted the code

--- src/App.js
+++ src/App.js
@@ -40,7 +40,6 @@ export default function Board(){
     <>
       <div className="status">{status}</div>
       <div className="board-row">
-        
         <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
         <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
